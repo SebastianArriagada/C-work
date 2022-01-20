cec0876f06738e5d0080a50444a5a9a29dc294e2
Sebastian Arriagada <sarriagada15@gmail.com>
2022-01-20 11:09:14 +0100

    Create folder for store Hacker Rank exercises

---
+++ hackerRank/Mini-Max-Sum.c
@@ -0,0 +1,73 @@
+/*
+
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Example
+
+The minimum sum is  and the maximum sum is . The function prints
+
+16 24
+Function Description
+
+Complete the miniMaxSum function in the editor below.
+
+miniMaxSum has the following parameter(s):
+
+arr: an array of  integers
+Print
+
+Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+Input Format
+
+A single line of five space-separated integers.
+
+Constraints
+
+
+Output Format
+
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+
+1 2 3 4 5
+Sample Output
+
+10 14
+Explanation
+
+The numbers are , , , , and . Calculate the following sums using four of the five integers:
+
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Sum everything except , the sum is .
+Hints: Beware of integer overflow! Use 64-bit Integer.
+
+
+*/
+
+int cmpfunc(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+
+void miniMaxSum(int arr_count, int *arr)
+{
+
+    // Sorting
+    qsort(arr, arr_count, sizeof(int), cmpfunc);
+
+    long min = 0;
+    long max = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+        min += arr[i];
+        max += arr[4 - i];
+    }
+
+    printf("%ld %ld", min, max);
+}
